06be50d4c4d59e0b37f6d3ab0954e724dbf26a99
xhishka <ivfed2006@mail.ru>
2023-01-29 12:57:17 +0500

    just save

--- dead/app1/models.py
+++ dead/app1/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Gifts(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.ImageField(upload_to='images/')
-    uurl = models.CharField('Название', max_length=200)
+
 
     def __str__(self):
         return self.title
