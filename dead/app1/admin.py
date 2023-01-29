b3b90933b2245c6662d32c3336dbbff3606e808b
xhishka <ivfed2006@mail.ru>
2023-01-29 12:53:10 +0500

    just save

--- dead/app1/admin.py
+++ dead/app1/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app1.models import Gifts
+from app1.models import Gifts, Basket
 
 
 admin.site.register(Gifts)
+admin.site.register(Basket)
--- dead/app1/models.py
+++ dead/app1/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class Gifts(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.ImageField(upload_to='images/')
+    uurl = models.CharField('Название', max_length=200)
 
+    def __str__(self):
+        return self.title
+
+
+class Basket(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.ImageField(upload_to='backet/')
     def __str__(self):
         return self.title
